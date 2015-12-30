a3385ac310efe68f9b9332cfa06175378a645cf6
HuyNQ <huy@huynq.net>
2015-12-30 11:12:55 +0700

    Add 4th exercise

---
+++ 04_modules_with_name.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import { PI, sqrt, square } from './04_math.js'
+
+let arg1 = process.argv[2]
+let arg2 = process.argv[3]
+
+console.log(PI)
+console.log(sqrt(+arg1))
+console.log(square(+arg2))
