d13a4252443a60f634a2cc3760b3c3e2faa6c64d
HuyNQ <huy@huynq.net>
2015-12-30 11:19:28 +0700

    Add 5th exercise

---
+++ 05_modules_default_export.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import Math from './05_math.js'
+
+let arg1 = process.argv[2]
+let arg2 = process.argv[3]
+
+console.log(Math.PI)
+console.log(Math.sqrt(+arg1))
+console.log(Math.square(+arg2))
